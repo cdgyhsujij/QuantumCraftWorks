bfe91c0fe036f61fd021725bdf326e3f67b441d4
cdgyhsujij <gsjcatosurfista@hotmail.com>
2024-05-06 19:52:30 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function serialize(root) {
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const node = queue.shift();
-    if (node) {
-      result.push(node.val);
-      queue.push(node.left, node.right);
-    } else {
-      result.push(null);
-    }
-  }
-  while (result[result.length - 1] === null) {
-    result.pop();
-  }
-  return result;
-}
+const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => !s.has(x));
+};
